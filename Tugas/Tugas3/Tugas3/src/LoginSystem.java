1cc4f73de5fc79f77707ccc94fb1c95176eb3a60
Jjep3105 <saverororo@gmail.com>
2025-03-24 23:44:07 +0700

    Add files via upload

---
+++ Tugas/Tugas3/Tugas3/src/LoginSystem.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+// Superclass User
+class User {
+    private String nama;
+    private String nim;
+
+    public User(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public boolean login(String inputNama, String inputNim) {
+        return false;
+    }
+
+    public void displayInfo() {
+        System.out.println("Informasi pengguna tidak tersedia.");
+    }
+}
+
+// Subclass Admin
+class Admin extends User {
+    private String username;
+    private String password;
+
+    public Admin(String nama, String nim, String username, String password) {
+        super(nama, nim);
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public boolean login(String inputUsername, String inputPassword) {
+        return this.username.equals(inputUsername) && this.password.equals(inputPassword);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Login Admin sukses! Selamat datang, " + getNama());
+    }
+}
+
+// Subclass Mahasiswa
+class Mahasiswa extends User {
+    public Mahasiswa(String nama, String nim) {
+        super(nama, nim);
+    }
+
+    @Override
+    public boolean login(String inputNama, String inputNim) {
+        return getNama().equals(inputNama) && getNim().equals(inputNim);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Login Mahasiswa sukses! Selamat datang, " + getNama());
+    }
+}
+
+// Kelas utama LoginSystem
+public class LoginSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Admin admin = new Admin("Admin User", "239", "Savero", "202410370110239");
+        Mahasiswa mahasiswa = new Mahasiswa("Savero", "239");
+
+        System.out.println("Pilih login sebagai:");
+        System.out.println("1. Admin");
+        System.out.println("2. Mahasiswa");
+        System.out.print("Masukkan pilihan: ");
+        int pilihan = scanner.nextInt();
+        scanner.nextLine();
+
+        if (pilihan == 1) {
+            System.out.print("Masukkan username: ");
+            String username = scanner.nextLine();
+            System.out.print("Masukkan password: ");
+            String password = scanner.nextLine();
+
+            if (admin.login(username, password)) {
+                admin.displayInfo();
+            } else {
+                System.out.println("Login Admin gagal! Username atau password salah.");
+            }
+        } else if (pilihan == 2) {
+            System.out.print("Masukkan nama: ");
+            String nama = scanner.nextLine();
+            System.out.print("Masukkan NIM: ");
+            String nim = scanner.nextLine();
+
+            if (mahasiswa.login(nama, nim)) {
+                mahasiswa.displayInfo();
+            } else {
+                System.out.println("Login Mahasiswa gagal! Nama atau NIM salah.");
+            }
+        } else {
+            System.out.println("Pilihan tidak valid!");
+        }
+
+        scanner.close();
+    }
+}
